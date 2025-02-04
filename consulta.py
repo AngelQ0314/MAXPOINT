f69d46bdc8bce3e0205743e77cb9a90707c9e7ae
AngelQ0314 <leonelquishpe0314@gmail.com>
2025-02-04 11:31:17 -0500

    Consulta a la base de datos

--- consulta.py
+++ consulta.py
@@ -1,19 +1,54 @@
 import pyodbc
 
-# Configura los detalles de tu base de datos
-server = '127.0.0.1'  
+# Configuracion a la base de datos
+server = '127.0.0.1'
 database = 'MAXPOINT'
 username = 'angel_0314'
 password = '0314'
-
-# Crear la conexión
 conn_str = f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
 
 try:
+    # Conectar a la base de datos
     conn = pyodbc.connect(conn_str)
-    print("Conexión exitosa a SQL Server")
+    cursor = conn.cursor()
+
+    query = """
+    WITH HoraCalculada AS (
+        SELECT Fecha, Hora, 
+               RIGHT(Fecha, 12) AS HoraDesdeFecha,  
+               Hora AS HoraOriginal,  
+               DATEDIFF(MINUTE, 
+                        TRY_CONVERT(TIME, RIGHT(Fecha, 12)),  
+                        TRY_CONVERT(TIME, Hora)  
+               ) AS Minutos_Transcurridos,
+               intFlag1,
+               intFlag2,
+               intFlag3
+        FROM dbo.UpdateStore
+    )
+    SELECT 
+        COUNT(CASE WHEN intFlag1 IS NULL THEN 1 END) AS Total_NULL_intFlag1,
+        COUNT(CASE WHEN intFlag1 IS NULL AND HC.Minutos_Transcurridos > 5 THEN 1 END) AS NULL_Mayor_5_Minutos_intFlag1,
+        COUNT(CASE WHEN intFlag2 IS NULL THEN 1 END) AS Total_NULL_intFlag2,
+        COUNT(CASE WHEN intFlag2 IS NULL AND HC.Minutos_Transcurridos > 5 THEN 1 END) AS NULL_Mayor_5_Minutos_intFlag2,
+        COUNT(CASE WHEN intFlag3 IS NULL THEN 1 END) AS Total_NULL_intFlag3,
+        COUNT(CASE WHEN intFlag3 IS NULL AND HC.Minutos_Transcurridos > 5 THEN 1 END) AS NULL_Mayor_5_Minutos_intFlag3
+    FROM HoraCalculada HC;
+    """
+
+    # Ejecutar la consulta
+    cursor.execute(query)
+
+    # Obtener los resultados
+    results = cursor.fetchone()
+
+    # Mostrar los resultados
+    print(f"-intFlag1 :",f"TOTAL DE REGISTROS NULOS : {results[0]} ; " , f"TOTAL DE REGISTROS NULOS MAYOR A 5 MINUTOS : {results[1]}")
+    print(f"-intFlag2 :",f"TOTAL DE REGISTROS NULOS : {results[2]} ; " , f"TOTAL DE REGISTROS NULOS MAYOR A 5 MINUTOS : {results[3]}")
+    print(f"-intFlag3 :",f"TOTAL DE REGISTROS NULOS : {results[4]} ; " , f"TOTAL DE REGISTROS NULOS MAYOR A 5 MINUTOS : {results[5]}")
+
+    # Cerrar conexión
     conn.close()
+
 except Exception as e:
     print(f"Error al conectar: {e}")
-
-
